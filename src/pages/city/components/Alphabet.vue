be58be6f2dd47bda8bb04a08a5a7b0be8767ca42
zb961026 <602153088@qq.com>
2018-12-13 16:59:37 +0800

    完成了第二页面

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="list">
+        <li class="item">A</li>
+        <li class="item">B</li>
+        <li class="item">C</li>
+        <li class="item">D</li>
+        <li class="item">E</li>
+        <li class="item">F</li>
+    </div>
+</template>
+
+<script>
+    
+</script>
+
+<style lang="stylus" scoped>
+@import '../../../assets/styles/varibles.styl'
+ .list
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    position:absolute;
+    top:1.58rem;
+    right:0;
+    bottom:0;
+    width:.4rem;
+    .item
+     line-height:.4rem;
+     text-align:center;
+     color:$bgColor;
+     list-style:none;
+
+</style>
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,12 +1,11 @@
 <template>
-    <div class="list">
+    <div class="list" ref="wrapper">
+        <div>
         <div class="area">
             <div class="title border-topbottom">当前城市</div>
             <div class="button-list">
                 <div class="button-wrapper">
-                    <div class="button">
-                        北京
-                    </div>
+                    <div class="button">北京</div>
                 </div>
             </div>
         </div>
@@ -32,7 +31,6 @@
                     <div class="button">
                         北京
                     </div>
-                </div>
             </div>
         </div>
         <div class="area">
@@ -101,13 +99,19 @@
                 <div class="item border-bottom">阿拉尔</div>
             </div>
         </div>
+        </div>
+    </div>
     </div>
     
 </template>
 
 <script>
+    import Bsroll from 'better-scroll'
     export default{
-        name:'CityList'
+        name:'CityList',
+        mounted() {
+            this.scroll=new Bsroll(this.$refs.wrapper)
+        },
     }
 </script>
 
