6cdce12b705f1ae1891a9b78cfba9dbfa517f957
zb961026 <602153088@qq.com>
2018-12-13 17:48:18 +0800

    第二页面完成

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="list">
-        <li class="item">A</li>
-        <li class="item">B</li>
-        <li class="item">C</li>
-        <li class="item">D</li>
-        <li class="item">E</li>
-        <li class="item">F</li>
+        <li class="item" v-for="(item,key) of cities" :key="key">{{key}}</li>
     </div>
 </template>
 
 <script>
-    
+  export default{
+      name:'CityAlphabet',
+      props:{
+          cities:Object
+      }
+  }  
 </script>
 
 <style lang="stylus" scoped>
